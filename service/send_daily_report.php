7981609cc8e7b1844dfd9851afd057388de923e9
pytharchimede <amani_ulrich@outlook.fr>
2025-01-23 23:42:23 +0000

    Fix envoie rapport journalier whatsapp

--- service/send_daily_report.php
+++ service/send_daily_report.php
@@ -4,14 +4,14 @@ require_once '../model/WhatsAppSMS.php';
 // Configuration Twilio
 $sid = "ACded19f6cd55b2ba3d18c13f438f1e878"; // Votre SID Twilio
 $token = "fea6684286c6b923e8ce0ce19bc48cb4"; // Remplacez par votre AuthToken
-$from = "+2250711048002"; // Numéro WhatsApp Twilio
+$from = "whatsapp:+2250711048002"; // Numéro WhatsApp Twilio
 
 // Créer une instance de WhatsAppSMS
 $whatsapp = new WhatsAppSMS($sid, $token, $from);
 
 // Informations du destinataire
 $number = "05055262";
-$number = "0748367710";
+// $number = "0748367710";
 
 $recipient_number = "+225" . $number; // Numéro formaté avec indicatif pays
 $recipient_name = "Alex BRAUD";
